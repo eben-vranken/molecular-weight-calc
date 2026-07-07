f62748df4f1bf89ff87dbfab0e6adeaf70cf25c1
eben-vranken <vranken.eben@outlook.com>
2026-07-07 23:09:10 +0200

    feat: parse elements and their counts formula

--- parser.py
+++ parser.py
@@ -1,2 +1,40 @@
-def parse_formula(formula):
-    pass
+import calculator
+
+def parse_formula(formula: str):
+    elements = {
+
+    }
+    active = False
+    current_elem = ""
+    for c in formula:
+        if c.isdigit() and active:
+            elements[current_elem] = int(c)
+            active = False
+            current_elem = ""
+            continue
+        
+        if c.isupper():
+            if active:
+                elements[c] = 1
+                current_elem = ""
+            else:
+                current_elem += c
+                active = True
+            continue
+        
+        current_elem += c
+    
+    if active:
+        elements[current_elem] = 1
+    
+    return elements
+
+
+if __name__ == "__main__":
+    test_formula = "H2O3Fe3"
+    test_elements = parse_formula(test_formula)
+
+    w = calculator.calculate_weight(test_elements)
+    
+    print(test_elements)
+    print(f"%.3fg" % w)
