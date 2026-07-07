93a97e9d93828e48db9ea510521b8966b58f136d
eben-vranken <vranken.eben@outlook.com>
2026-07-07 23:19:59 +0200

    refactor: tighten formula parser to catch edge cases

--- parser.py
+++ parser.py
@@ -4,37 +4,41 @@ def parse_formula(formula: str):
     elements = {
 
     }
-    active = False
     current_elem = ""
+    current_count = ""
+
     for c in formula:
-        if c.isdigit() and active:
-            elements[current_elem] = int(c)
-            active = False
-            current_elem = ""
+        if c.isdigit():
+            current_count += c
             continue
         
         if c.isupper():
-            if active:
-                elements[c] = 1
-                current_elem = ""
-            else:
-                current_elem += c
-                active = True
+            if current_elem:
+                if not current_count:
+                    current_count = "1"
+                
+                elements[current_elem] = int(current_count)
+                
+            current_elem = c
+            current_count = ""
             continue
-        
+    
         current_elem += c
     
-    if active:
-        elements[current_elem] = 1
+    if current_elem:
+        if not current_count:
+            current_count = "1"
+        elements[current_elem] = int(current_count)
     
     return elements
 
 
 if __name__ == "__main__":
-    test_formula = "H2O3Fe3"
+    test_formula = "Fe13HO5"
     test_elements = parse_formula(test_formula)
+
+    print(test_elements)
 
     w = calculator.calculate_weight(test_elements)
     
-    print(test_elements)
     print(f"%.3fg" % w)
